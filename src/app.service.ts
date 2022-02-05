705a2f06bd388b7dcb9295bfca676a2f0abd820a
Nelson <nelsonalejandroramosrivera@gmail.com>
2022-02-05 10:14:48 -0300

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -14,20 +14,18 @@ export class AppService {
   } */
   writeMessage(body: Body): any {
     try {
-      
-      //validaciones
-      if (body['message'] == undefined) return "formato incorrecto, enviame un json con la parametro message"
-      if (body['message'].length == 0) return "el mensaje no puede venir vacio"
-      
       //lectura del fichero
       var bd: any = JSON.parse(fs.readFileSync('bd.json', 'utf-8'));
-      
-      //escritura del mensaje
-      bd.push({ "id": bd.length+1, "message": body['message'], "createAt": new Date('2/1/22') })
-      fs.writeFile('bd.json', JSON.stringify(bd), (err) => {
-        if (err) throw "error escribiendo datos";
-        console.log('mensaje guardado');
-      });
+
+      //validaciones
+      if (body['message'] != undefined && body['message'].length != 0) {
+        //escritura del mensaje
+        bd.push({ "id": bd.length + 1, "message": body['message'], "createAt": new Date('2/1/22') })
+        fs.writeFile('bd.json', JSON.stringify(bd), (err) => {
+          if (err) throw "error";
+          console.log('mensaje guardado');
+        });
+      }
     } catch (error) {
       return "Error escribiendo los datos"
     }
