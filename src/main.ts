9dc14797bfc35062f46a5c7af231575bb59a74e2
Nelson <nelsonalejandroramosrivera@gmail.com>
2022-02-05 14:27:02 -0300

    change coors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = {
+    origin: '*',
+    methods: 'POST',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  };
+  /* console.log(app); */
+  app.enableCors(options);
   await app.listen(3000);
 }
 bootstrap();
